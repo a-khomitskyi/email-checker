af8211ebe37772b0d241b1fa50686911ca8b0797
a-khomitskyi <homitskiy.andriy@gmail.com>
2022-03-16 23:12:40 +0200

    Code review

--- checker.py
+++ checker.py
@@ -8,15 +8,19 @@ URL = 'https://gist.githubusercontent.com/michenriksen/8710649/raw/e09ee253960ec
 res = get(URL).content
 
 
-def is_actual(file: object) -> bool:
-	"""Check file's last time update. Function will return True if date is actual else False"""
+def _is_actual(file: object) -> bool:
+	"""Check file's last time update
+	:param file: Name of temporary domain file
+	:return: Function will return True if date is actual else False"""
 	return True if str(datetime.fromtimestamp(getmtime(file)).date()) == str(
 		datetime.today().strftime('%Y-%m-%d')) else False
 
 
-def update_db(url: str) -> None:
-	"""Update disposable email domains database. Will update one a day"""
-	if not is_actual('resource.txt'):
+def _update_db(url: str) -> None:
+	"""Update disposable email domains database. Will update one a day
+	:param url: Link on text file of domain
+	:return: None"""
+	if not _is_actual('resource.txt'):
 
 		res = get(URL).text
 
@@ -25,15 +29,18 @@ def update_db(url: str) -> None:
 				f.write(i)
 
 
-def quick_check(email: str) -> bool:
-	"""Email fast check using regexp (available patterns)"""
-	update_db(URL)
+def quick_check(email: str) -> dict:
+	"""Email fast check using regexp (available patterns)
+	:param email: E-mail address"""
+	_update_db(URL)
 	pattern = re.compile(r"^[a-zA-Z0-9]{1}[a-zA-Z0-9_.%-+]{,63}@[a-zA-Z0-9]{1}[a-zA-Z0-9.-]{0,}\.[a-z|A-Z]{2,}]{,254}$")
-	return True if re.match(pattern, email) else False
+	return {'is_valid': True, 'email': email} if re.match(pattern, email) else {'is_valid': False, 'email': email}
 
 
-def is_disposable(email: str) -> bool:
-	"""Make sure that email isn't disposable. Function will return True if email is disposable else return False"""
+def _is_disposable(email: str) -> bool:
+	"""Make sure that email isn't disposable.
+	:param email: E-mail address
+	:return: Function will	return True if email is disposable else return False"""
 	domain = email.split('@')[1]
 
 	with open('resource.txt') as f:
@@ -45,7 +52,9 @@ def is_disposable(email: str) -> bool:
 
 
 def ping_mail(email):
-	"""Check email exists using foreign API for SMTP ping"""
+	"""Check email exists using foreign API for SMTP ping
+	:param email: E-mail address
+	:returns: False if e-mail is invalid; True if e-mail is valid; -1 if e-mail isn't verify"""
 	response = get("https://isitarealemail.com/api/email/validate", params={'email': email})
 
 	status = response.json()['status']
@@ -59,17 +68,15 @@ def ping_mail(email):
 
 
 def deep_check(email: str) -> dict:
-	"""
-	Email deeply check.
-	return: True if email exists
-			False if email incorrect
-			-1 if system can't verify email
-	"""
+	"""Email deeply check. return: True if email exists
+	False if email incorrect -1 if system can't verify email
+	:param email: E-mail address
+	:returns: False if e-mail is invalid; True if e-mail is valid; -1 if e-mail isn't verify"""
 
 	try:
 		if not quick_check(email):
 			raise IncorrectEmailException('Incorrect e-mail address!')
-		if is_disposable(email):
+		if _is_disposable(email):
 			raise DisposableEmailException('E-mail is disposable! Please, use your real mail')
 		ping_mail(email)
 	except IncorrectEmailException as _e:
@@ -81,5 +88,4 @@ def deep_check(email: str) -> dict:
 
 
 if __name__ == '__main__':
-	# update_db(URL)
 	print(deep_check('ganjuibas@gmail.com'))
